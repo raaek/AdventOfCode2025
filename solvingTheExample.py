f45395a6044bcaea47b225441730101a9347694d
raaek <Ruth.Kaiser@ymail.com>
2025-12-03 00:00:18 +0100

    first solution for day 1, second puzzle

--- solvingTheExample.py
+++ solvingTheExample.py
@@ -1,18 +1,20 @@
 import numpy as np
+import math
 
 # translate the original rotaions (e.g. L30) into numbers with plus and minus (e.g. -30)
 def translate_rotations_into_numbers(original_rotations):
-    print(original_rotations)
+    # print(original_rotations)
     rotations = [-int(elem.replace("L","")) if "L" in elem else int(elem.replace("R","")) for elem in  original_rotations]
     return rotations
 
 # calculate the new dial setting from the old one and the rotation
 def calculate_new_dial_setting(current_dial_setting, rotation):
     new_dial_setting = (current_dial_setting + rotation)%100
-    print(new_dial_setting)
+    # print(new_dial_setting)
     return new_dial_setting
 
 def main():
+
     number_of_times_0_has_been_reached = 0 # will be the password
     initial_dial_setting = 50
     current_dial_setting = initial_dial_setting
@@ -27,13 +29,22 @@ def main():
 
     rotations = translate_rotations_into_numbers(original_rotations)
 
+    print("the dial starts by pointing at ", current_dial_setting)
     for rotation in rotations:
-        current_dial_setting = calculate_new_dial_setting(current_dial_setting, rotation)
-        if current_dial_setting == 0:
-            number_of_times_0_has_been_reached+=1
+        sum = current_dial_setting + rotation
+        print("the dial is rotated ", rotation, "to point at ", sum%100)
+        if sum>=100:
+            number_of_times_0_has_been_reached = number_of_times_0_has_been_reached + math.floor(sum/100)
+            print("during this rotation, it points at 0 ", math.floor(sum/100), " times")
+        if sum<=0:
+            number_of_times_0_has_been_reached = number_of_times_0_has_been_reached + 1 + math.floor(np.abs(sum)/100)
+            print("during this rotation, it points at 0 ", 1 + math.floor(np.abs(sum)/100), " times")
+            if current_dial_setting == 0:
+                number_of_times_0_has_been_reached = number_of_times_0_has_been_reached - 1
 
-    print("password:\n")
-    print(number_of_times_0_has_been_reached)
+        current_dial_setting = calculate_new_dial_setting(current_dial_setting, rotation)
+
+    print("password:\n", number_of_times_0_has_been_reached)
 
 if __name__=="__main__":
     main()
