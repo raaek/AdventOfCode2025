4be485b57e33f65252c80055c4d4d6bdc2c20cbe
raaek <Ruth.Kaiser@ymail.com>
2025-12-04 08:04:49 +0100

    solution part 1 day 3

--- Day3.py
+++ Day3.py
@@ -1,7 +1,7 @@
 from asyncio.log import logger
 import logging
 
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(level=logging.DEBUG)
 
 
 class batteryBank:
@@ -10,7 +10,24 @@ class batteryBank:
 
     def _get_highest_joltage(self) -> int:
         bank = self.bank
-        highest_joltage = highest_digit_in(bank)
+        logger.debug("bank: %s", bank)
+        highest_digit = highest_digit_in(bank)
+        if bank.index(str(highest_digit)) == len(bank)-1:
+            logger.debug("highest digit (%i) in last place", highest_digit)
+            second_highest_digit = highest_digit
+            updated_bank = bank[:-1]
+            logger.debug("updated bank: %s", updated_bank)
+            first_highest_digit = highest_digit_in(updated_bank)
+        else:
+            first_highest_digit = highest_digit
+            index = bank.index(str(highest_digit))
+            logger.debug("highest digit (%i) in %i place", highest_digit, index)
+            updated_bank = bank[index+1:]
+            logger.debug("updated bank: %s", updated_bank)
+            second_highest_digit = highest_digit_in(updated_bank)
+            logger.debug("second highest digit (%i)", second_highest_digit)
+        highest_joltage = int(str(first_highest_digit) + str(second_highest_digit))
+        logger.debug("highest joltage: %i", highest_joltage)
         return highest_joltage
 
 
@@ -22,10 +39,15 @@ def highest_digit_in(sequence: str) -> int:
 
 def main() -> None:
     input_file: str = "day3_input.txt"
+    total_output_joltage: int = 0
     with open(input_file) as file:
         for line in file.readlines():
             bank = batteryBank(line.rstrip())
-            logger.info("highest digit in bank: %i", bank._get_highest_joltage())
+            highest_joltage = bank._get_highest_joltage()
+            # logger.info("highest joltage in bank: %i", highest_joltage)
+            total_output_joltage = total_output_joltage + highest_joltage
+    
+    logger.info("total output joltage %i", total_output_joltage)
 
 if __name__ == "__main__":
     main()
