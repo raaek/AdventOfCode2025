46fad39652d783b2d42289dd83ad7fd2cd41d1a6
raaek <Ruth.Kaiser@ymail.com>
2025-12-04 07:37:30 +0100

    first skeleton of day3 puzzle

--- Day3.py
+++ Day3.py
@@ -1,4 +1,31 @@
+from asyncio.log import logger
+import logging
 
-class batteryBank():
+logging.basicConfig(level=logging.INFO)
+
+
+class batteryBank:
     def __init__(self, bank: str):
         self.bank = bank
+
+    def _get_highest_joltage(self) -> int:
+        bank = self.bank
+        highest_joltage = highest_digit_in(bank)
+        return highest_joltage
+
+
+def highest_digit_in(sequence: str) -> int:
+    highest_digit = max([int(digit) for digit in sequence])
+    logger.debug(highest_digit)
+    return highest_digit
+
+
+def main() -> None:
+    input_file: str = "day3_input.txt"
+    with open(input_file) as file:
+        for line in file.readlines():
+            bank = batteryBank(line.rstrip())
+            logger.info("highest digit in bank: %i", bank._get_highest_joltage())
+
+if __name__ == "__main__":
+    main()
