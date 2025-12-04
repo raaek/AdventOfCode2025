6760af7d6a210e69b08f946fee2bf66f9877aa27
raaek <Ruth.Kaiser@ymail.com>
2025-12-04 02:25:17 +0100

    solution day 2 puzzle 2

--- Day2.py
+++ Day2.py
@@ -17,14 +17,42 @@ class product_ID_interval:
     def _get_last_ID(self) -> int:
         return self.last_ID
 
+    # TODO throw out numbers of odd length
+    def _find_invalid_IDS(self) -> int:
+        invalid_IDs: list[int] = []
+        first_ID = self.first_ID
+        last_ID = self.last_ID
+        for id in range(first_ID, last_ID + 1):
+            length_ID = len(str(id))
+            divisors: list[int] = []
+            for j in range(1, int(length_ID / 2) + 1):
+                if is_divisor(j, length_ID):
+                    divisors.append(j)
+            for k in divisors:
+                id_str = str(id)
+                id_temp = id_str.replace(id_str[:k], "")
+                if id_temp == "":
+                    invalid_IDs.append(id)
+                    break
+
+        return sum(invalid_IDs)
+
+
+def is_divisor(divisor: int, dividend: int) -> bool:
+    if (dividend / divisor) == (dividend // divisor):
+        is_divisor = True
+    else:
+        is_divisor = False
+    return is_divisor
+
 
 def main() -> None:
     input_file: str = "day2_input.txt"
     product_ID_intervals: list[str] = []
+    total_of_invalid_IDs: int = 0
     with open(input_file) as file:
         for line in file.readlines():
-            # rewrite with extend
-            product_ID_intervals = line.split(sep=",")
+            product_ID_intervals.extend(line.split(sep=","))
     logger.info(product_ID_intervals)
 
     for interval_str in product_ID_intervals:
@@ -34,6 +62,12 @@ def main() -> None:
         last_ID = interval._get_last_ID()
 
         logger.debug("first ID %i, last ID %i", first_ID, last_ID)
+
+        invalid_IDs = interval._find_invalid_IDS()
+        total_of_invalid_IDs = total_of_invalid_IDs + invalid_IDs
+        print(total_of_invalid_IDs)
+
+        logger.info("total sum of invalid IDs %i", total_of_invalid_IDs)
 
 
 if __name__ == "__main__":
